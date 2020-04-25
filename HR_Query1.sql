13646bc89a7d25a8043e44bf1493429ab7f87dde
Louhilton <57717964+Louhilton01@users.noreply.github.com>
2020-04-25 15:19:39 +0700

    Add files via upload

---
+++ HR_Query1.sql
@@ -0,0 +1,7 @@
+--Problem at https://www.hackerrank.com/challenges/revising-the-select-query/problem
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+select * from city where countrycode='USA' and population>100000;
---
+++ HR_Query2.sql
@@ -0,0 +1,2 @@
+--Problem at https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+select name from city where countrycode='USA' and population>120000;
